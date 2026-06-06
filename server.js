4830e35a42765a17a67a7284bb005694e663847a
913122104174_Swetha <121333524+028-SWETHA@users.noreply.github.com>
2026-06-06 15:29:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -80,4 +80,6 @@ app.post("/login", async (req, res) => {
 
 // ✅ Dynamic PORT (IMPORTANT)
 const PORT = process.env.PORT || 5000;
+app.get("/ping", (req, res) => res.send("pong"));
+
 app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
